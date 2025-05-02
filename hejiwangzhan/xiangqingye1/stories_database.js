eb6c7452d568d825c5b2d2fa708384fd688f0955
fufuai <ttp38933@gmail.com>
2025-05-02 18:51:40 +0800

    提交链接1.0

---
+++ hejiwangzhan/xiangqingye1/stories_database.js
@@ -0,0 +1,374 @@
+// 调教故事数据库
+const storiesDatabase = {
+    // 痴女调教故事
+    masochistStories: [
+        {
+            id: "ms001",
+            title: "美诺子的调教日记",
+            author: "未知",
+            category: "痴女调教",
+            tags: ["痴女", "调教", "性奴"],
+            content: "美诺子跪在你面前，双腿微微颤抖，她的身体已经准备好接受你的调教。主人...人家的下面好湿...好想要主人的大肉棒插人家的小穴...请狠狠地惩罚人家...",
+            characterInfo: {
+                name: "美诺子",
+                age: 22,
+                height: 165,
+                weight: 48,
+                measurements: "88-56-86",
+                breastSize: "D罩杯",
+                personality: "温柔、顺从、渴望被调教",
+                background: "美诺子是一名大学生，外表清纯可爱，但内心隐藏着强烈的欲望。她渴望被强者征服，渴望被调教成性奴。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "ms001_s01",
+                    title: "初次见面",
+                    content: "美诺子跪在你面前，双腿微微颤抖，她的身体已经准备好接受你的调教。主人...人家的下面好湿...好想要主人的大肉棒插人家的小穴...请狠狠地惩罚人家..."
+                },
+                {
+                    id: "ms001_s02",
+                    title: "手指调教",
+                    content: "你的手指轻轻探入美诺子的小穴，她立刻发出甜美的呻吟。啊...主人...你的手指好厉害...人家的小穴好舒服...请再深入一点...人家是主人的小母狗..."
+                },
+                {
+                    id: "ms001_s03",
+                    title: "乳房调教",
+                    content: "你的手掌覆盖在美诺子的乳房上，用力揉捏着，她的乳头已经硬挺起来。主人...人家的奶子好涨...请用力揉捏它们...乳头都硬起来了...人家是主人的性奴..."
+                }
+            ]
+        },
+        {
+            id: "ms002",
+            title: "樱子的堕落",
+            author: "未知",
+            category: "痴女调教",
+            tags: ["痴女", "调教", "性奴", "堕落"],
+            content: "樱子原本是一个清纯的女大学生，但在一次偶然的机会下，她遇到了你。你发现她内心隐藏着强烈的欲望，于是开始一步步引导她走向堕落...",
+            characterInfo: {
+                name: "樱子",
+                age: 20,
+                height: 158,
+                weight: 45,
+                measurements: "85-54-84",
+                breastSize: "C罩杯",
+                personality: "清纯、害羞、内心渴望堕落",
+                background: "樱子是一名大学生，外表清纯可爱，但内心隐藏着强烈的欲望。她渴望被强者征服，渴望被调教成性奴。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "ms002_s01",
+                    title: "初次相遇",
+                    content: "樱子走在校园里，突然被一个陌生人拦住。她感到害怕，但内心却有一种奇怪的期待。'你...你是谁？'她颤抖着问道。"
+                },
+                {
+                    id: "ms002_s02",
+                    title: "欲望觉醒",
+                    content: "你轻轻抚摸樱子的脸颊，她感到一阵酥麻传遍全身。'我...我这是怎么了...'她轻声呻吟道，身体不自觉地靠近你。"
+                },
+                {
+                    id: "ms002_s03",
+                    title: "初次调教",
+                    content: "你带着樱子来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                }
+            ]
+        }
+    ],
+
+    // 学生被调教成性便器故事
+    studentStories: [
+        {
+            id: "ss001",
+            title: "优子的转变",
+            author: "未知",
+            category: "学生调教",
+            tags: ["学生", "调教", "性便器", "转变"],
+            content: "优子是一个成绩优秀的高中生，但在一次意外中，她成为了你的性便器。你开始对她进行严格的调教，让她逐渐适应这个新的身份...",
+            characterInfo: {
+                name: "优子",
+                age: 17,
+                height: 160,
+                weight: 46,
+                measurements: "86-55-85",
+                breastSize: "C罩杯",
+                personality: "聪明、乖巧、渴望被征服",
+                background: "优子是一名高中生，成绩优秀，是老师眼中的好学生。但在一次意外中，她成为了你的性便器。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "ss001_s01",
+                    title: "意外相遇",
+                    content: "优子放学后独自走在回家的路上，突然被一个陌生人拦住。她感到害怕，但内心却有一种奇怪的期待。'你...你是谁？'她颤抖着问道。"
+                },
+                {
+                    id: "ss001_s02",
+                    title: "初次调教",
+                    content: "你带着优子来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                },
+                {
+                    id: "ss001_s03",
+                    title: "适应新身份",
+                    content: "经过一段时间的调教，优子逐渐适应了性便器的身份。'主人...人家的下面好湿...请继续调教我...'她跪在你面前，轻声说道。"
+                }
+            ]
+        },
+        {
+            id: "ss002",
+            title: "美咲的堕落",
+            author: "未知",
+            category: "学生调教",
+            tags: ["学生", "调教", "性便器", "堕落"],
+            content: "美咲是一个天真烂漫的初中生，但在一次意外中，她成为了你的性便器。你开始对她进行严格的调教，让她逐渐适应这个新的身份...",
+            characterInfo: {
+                name: "美咲",
+                age: 15,
+                height: 155,
+                weight: 43,
+                measurements: "84-53-83",
+                breastSize: "B罩杯",
+                personality: "天真、烂漫、渴望被征服",
+                background: "美咲是一名初中生，天真烂漫，是老师眼中的好学生。但在一次意外中，她成为了你的性便器。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "ss002_s01",
+                    title: "初次相遇",
+                    content: "美咲放学后独自走在回家的路上，突然被一个陌生人拦住。她感到害怕，但内心却有一种奇怪的期待。'你...你是谁？'她颤抖着问道。"
+                },
+                {
+                    id: "ss002_s02",
+                    title: "初次调教",
+                    content: "你带着美咲来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                },
+                {
+                    id: "ss002_s03",
+                    title: "适应新身份",
+                    content: "经过一段时间的调教，美咲逐渐适应了性便器的身份。'主人...人家的下面好湿...请继续调教我...'她跪在你面前，轻声说道。"
+                }
+            ]
+        }
+    ],
+
+    // 大学生被下药成性奴故事
+    collegeStories: [
+        {
+            id: "cs001",
+            title: "爱理的迷药",
+            author: "未知",
+            category: "大学生调教",
+            tags: ["大学生", "下药", "性奴", "迷药"],
+            content: "爱理是一个普通的大学生，在一次派对上，她不小心喝下了被下药的饮料。药效发作后，她变得异常敏感和淫荡，成为了你的性奴...",
+            characterInfo: {
+                name: "爱理",
+                age: 21,
+                height: 162,
+                weight: 47,
+                measurements: "87-55-86",
+                breastSize: "C罩杯",
+                personality: "活泼、开朗、渴望被征服",
+                background: "爱理是一名大学生，活泼开朗，是朋友眼中的开心果。但在一次派对上，她不小心喝下了被下药的饮料，成为了你的性奴。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "cs001_s01",
+                    title: "派对相遇",
+                    content: "爱理参加了一个朋友的派对，不小心喝下了被下药的饮料。'这饮料好奇怪...'她感到头晕目眩，身体开始发热。"
+                },
+                {
+                    id: "cs001_s02",
+                    title: "药效发作",
+                    content: "药效开始发作，爱理感到异常敏感和淫荡。'好热...好痒...'她轻声呻吟道，身体不自觉地靠近你。"
+                },
+                {
+                    id: "cs001_s03",
+                    title: "成为性奴",
+                    content: "你带着爱理来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                }
+            ]
+        },
+        {
+            id: "cs002",
+            title: "美月的迷药",
+            author: "未知",
+            category: "大学生调教",
+            tags: ["大学生", "下药", "性奴", "迷药"],
+            content: "美月是一个普通的大学生，在一次派对上，她不小心喝下了被下药的饮料。药效发作后，她变得异常敏感和淫荡，成为了你的性奴...",
+            characterInfo: {
+                name: "美月",
+                age: 20,
+                height: 163,
+                weight: 48,
+                measurements: "88-56-87",
+                breastSize: "D罩杯",
+                personality: "温柔、善良、渴望被征服",
+                background: "美月是一名大学生，温柔善良，是朋友眼中的知心姐姐。但在一次派对上，她不小心喝下了被下药的饮料，成为了你的性奴。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "cs002_s01",
+                    title: "派对相遇",
+                    content: "美月参加了一个朋友的派对，不小心喝下了被下药的饮料。'这饮料好奇怪...'她感到头晕目眩，身体开始发热。"
+                },
+                {
+                    id: "cs002_s02",
+                    title: "药效发作",
+                    content: "药效开始发作，美月感到异常敏感和淫荡。'好热...好痒...'她轻声呻吟道，身体不自觉地靠近你。"
+                },
+                {
+                    id: "cs002_s03",
+                    title: "成为性奴",
+                    content: "你带着美月来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                }
+            ]
+        }
+    ],
+
+    // 其他类型的故事
+    otherStories: [
+        {
+            id: "os001",
+            title: "美雪的调教",
+            author: "未知",
+            category: "其他",
+            tags: ["调教", "性奴", "其他"],
+            content: "美雪是一个普通的上班族，但在一次意外中，她成为了你的性奴。你开始对她进行严格的调教，让她逐渐适应这个新的身份...",
+            characterInfo: {
+                name: "美雪",
+                age: 25,
+                height: 168,
+                weight: 50,
+                measurements: "90-58-89",
+                breastSize: "E罩杯",
+                personality: "成熟、稳重、渴望被征服",
+                background: "美雪是一名上班族，成熟稳重，是同事眼中的女强人。但在一次意外中，她成为了你的性奴。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "os001_s01",
+                    title: "初次相遇",
+                    content: "美雪下班后独自走在回家的路上，突然被一个陌生人拦住。她感到害怕，但内心却有一种奇怪的期待。'你...你是谁？'她颤抖着问道。"
+                },
+                {
+                    id: "os001_s02",
+                    title: "初次调教",
+                    content: "你带着美雪来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                },
+                {
+                    id: "os001_s03",
+                    title: "适应新身份",
+                    content: "经过一段时间的调教，美雪逐渐适应了性奴的身份。'主人...人家的下面好湿...请继续调教我...'她跪在你面前，轻声说道。"
+                }
+            ]
+        }
+    ],
+
+    // 新增：详细背景故事
+    detailedStories: [
+        {
+            id: "ds001",
+            title: "美咲的调教日记",
+            author: "未知",
+            category: "详细背景",
+            tags: ["调教", "性奴", "详细背景"],
+            content: "美咲是一名大学生，身高165cm，体重48kg，三围88-56-86，D罩杯。她有着一头乌黑的长发，白皙的肌肤，大大的眼睛，小巧的鼻子，樱桃般的小嘴。她的乳房丰满挺拔，乳头粉嫩，乳晕小巧。她的腰肢纤细，臀部圆润，大腿修长，小腿纤细。她的阴户粉嫩，阴蒂小巧，阴唇紧闭。她的后庭紧致，肛门粉嫩。她性格温柔，善良，渴望被征服。在一次偶然的机会下，她成为了你的性奴...",
+            characterInfo: {
+                name: "美咲",
+                age: 20,
+                height: 165,
+                weight: 48,
+                measurements: "88-56-86",
+                breastSize: "D罩杯",
+                personality: "温柔、善良、渴望被征服",
+                background: "美咲是一名大学生，身高165cm，体重48kg，三围88-56-86，D罩杯。她有着一头乌黑的长发，白皙的肌肤，大大的眼睛，小巧的鼻子，樱桃般的小嘴。她的乳房丰满挺拔，乳头粉嫩，乳晕小巧。她的腰肢纤细，臀部圆润，大腿修长，小腿纤细。她的阴户粉嫩，阴蒂小巧，阴唇紧闭。她的后庭紧致，肛门粉嫩。她性格温柔，善良，渴望被征服。",
+                sensitiveAreas: ["乳头", "阴蒂", "G点", "后庭"]
+            },
+            scenes: [
+                {
+                    id: "ds001_s01",
+                    title: "初次相遇",
+                    content: "美咲走在校园里，突然被一个陌生人拦住。她感到害怕，但内心却有一种奇怪的期待。'你...你是谁？'她颤抖着问道。"
+                },
+                {
+                    id: "ds001_s02",
+                    title: "初次调教",
+                    content: "你带着美咲来到一个隐秘的房间，开始对她进行调教。'啊...不要...'她嘴上说着不要，身体却诚实地回应着你的每一个动作。"
+                },
+                {
+                    id: "ds001_s03",
+                    title: "适应新身份",
+                    content: "经过一段时间的调教，美咲逐渐适应了性奴的身份。'主人...人家的下面好湿...请继续调教我...'她跪在你面前，轻声说道。"
+                }
+            ]
+        }
+    ]
+};
+
+// 获取所有故事
+function getAllStories() {
+    const allStories = [];
+
+    // 合并所有类别的故事
+    for (const category in storiesDatabase) {
+        allStories.push(...storiesDatabase[category]);
+    }
+
+    return allStories;
+}
+
+// 根据ID获取故事
+function getStoryById(id) {
+    for (const category in storiesDatabase) {
+        const story = storiesDatabase[category].find(s => s.id === id);
+        if (story) return story;
+    }
+    return null;
+}
+
+// 根据类别获取故事
+function getStoriesByCategory(category) {
+    return storiesDatabase[category] || [];
+}
+
+// 根据标签获取故事
+function getStoriesByTag(tag) {
+    const stories = [];
+
+    for (const category in storiesDatabase) {
+        const categoryStories = storiesDatabase[category].filter(s => s.tags.includes(tag));
+        stories.push(...categoryStories);
+    }
+
+    return stories;
+}
+
+// 搜索故事
+function searchStories(keyword) {
+    const stories = [];
+
+    for (const category in storiesDatabase) {
+        const categoryStories = storiesDatabase[category].filter(s =>
+            s.title.includes(keyword) ||
+            s.content.includes(keyword) ||
+            s.tags.some(tag => tag.includes(keyword))
+        );
+        stories.push(...categoryStories);
+    }
+
+    return stories;
+}
+
+// 导出数据库和函数
+export {
+    storiesDatabase,
+    getAllStories,
+    getStoryById,
+    getStoriesByCategory,
+    getStoriesByTag,
+    searchStories
+}; 
